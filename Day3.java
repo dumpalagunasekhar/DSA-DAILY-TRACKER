0fd97aa517b7e7a5e2c1376ab10441bf31257e38
gunasekhar dumpala <dumpalagunasekhar@gmsil.com>
2026-01-17 08:46:19 +0530

    Day3 complete Sucessfuly

---
+++ Day3.java
@@ -0,0 +1,22 @@
+class Pattern{
+    void pattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            for(int j=1;j<2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=i;j>=1;j--){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
+public class Day3{
+    public static void main(String[] args){
+        Pattern obj = new Pattern();
+        obj.pattern(5);
+    }
+}
--- README.md
+++ README.md
@@ -5,3 +5,11 @@ My DSA Consistency for become a expert in DSA
 ## DAY1
 
 Day1 on Patterns
+
+## DAY2
+
+Still on Logic Building
+
+## DAY3
+
+Getting Used to Logic
